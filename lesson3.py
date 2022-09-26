9271b07dc1a9b78513e2479e526a28dcca60a327
menyuu <101458830+menyuu@users.noreply.github.com>
2022-09-26 19:56:07 +0900

    Pythonの学習

--- lesson3.py
+++ lesson3.py
@@ -32,3 +32,72 @@ if a > 0:
     if b > 0:
         print('b is positive')
 
+a = 1
+b = 1
+
+# a が b と等しい
+a == b
+
+# a が b と異なる
+a != b
+
+# a が b よりも小さい
+a < b
+
+# a が b よりも大きい
+a > b
+
+# a が b 以下である
+a <= b
+
+# a が b 以上である
+a >= b
+
+# a も b も真であれば真
+a > 0 and b > 0
+
+# a または b が真であれば真
+a > 0 or b > 0
+
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+
+if 100 not in y:
+    print('not in')
+
+# a = 1
+# b = 2
+#
+# if not a == b:
+#     print('Not equal')
+#
+# if a != b:
+#     print('Not equal')
+
+is_ok = True
+
+if not is_ok:
+    print('hello')
+
+# False, 0, 0.0, '', [](リスト), ()(タプル), {}(辞書), set()(集合) といったものに何も入っていなければFalseを返す
+is_ok = []
+
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
+
+# None なにも値が入っていない
+is_empty = None
+# print(is_empty)
+if is_empty is None:
+    print('None!!!')
+
+print(1 == True)
+# オブジェクト同士が真であるか
+print(1 is True)
+# Noneオブジェクトかどうか
+print(None is None)
