2fd153db079e46b42ae19f5075af82763d7f2e33
menyuu <101458830+menyuu@users.noreply.github.com>
2022-10-03 02:08:26 +0900

    Pythonの学習

--- review/lesson4/lesson4-8.py
+++ review/lesson4/lesson4-8.py
@@ -1,1 +1,27 @@
 # 例外処理
+# エラーが発生した時に行う処理
+# try-except文
+l = [1, 2, 3]
+i = 5
+# try と except の間でエラーが発生した場合に except の処理を行う
+try:
+    l[i]
+except:
+    print("Don't worry")
+# 最後まで処理が行われる
+print('last')
+
+# 特定のエラーが発生した場合に処理を実行することもできる
+# except の後に特定のエラーを記述する
+# IndexError
+try:
+    l[i]
+except IndexError:
+    print("Don't worry")
+
+# as の後ろに変数名を書くことによってエラーの内容を格納することができる
+try:
+    l[i]
+except IndexError as ex:
+    # print(f"Don't worry: {ex}")
+    print("Don't worry: {}".format(ex))
