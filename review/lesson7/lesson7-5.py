bed6583b8e773a2952392a29290ff038ce51a65d
menyuu <101458830+menyuu@users.noreply.github.com>
2022-10-09 02:47:52 +0900

    Pythonの学習

--- review/lesson7/lesson7-5.py
+++ review/lesson7/lesson7-5.py
@@ -22,3 +22,38 @@ import tarfile
 with tarfile.open('test.tar.gz', 'r:gz') as tr:
     with tr.extractfile('test_dir/sub_dir/sub_test.txt') as f:
         print(f.read())
+
+# zipファイルの圧縮と展開
+# zipfileライブラリ
+# with文を書いて writeメソッドを使う
+# zipファイルはディレクトリだけでなくその中身まで指定する必要がある
+# ディレクトリのみを指定したら空のディレクトリが圧縮されるだけになる
+import zipfile
+
+with zipfile.ZipFile('test.zip', 'w') as z:
+    z.write('test_dir')
+    z.write('test_dir/test.txt')
+
+# zipの確認
+# globをインポートし一気にディレクトリやファイルを指定する
+import zipfile
+import glob
+
+with zipfile.ZipFile('test.zip', 'w') as z:
+    # アスタリスクを2つ付けることで recursive(再帰的)に確認できる
+    for f in glob.glob('test_dir/**', recursive=True):
+        print(f)
+        z.write(f)
+
+# zipファイルの展開
+# extractall でディレクトリを指定して展開する
+import zipfile
+with zipfile.ZipFile('test.zip', 'r') as z:
+    z.extractall('zzz2')
+
+# zipファイルを展開せずに中身を確認
+import zipfile
+
+with zipfile.ZipFile('test.zip', 'r') as z:
+    with z.open('test_dir/test.txt') as f:
+        print(f.read())
