1947ce7523e77d98b5905bb6ab2490b1420c1bcf
menyuu <101458830+menyuu@users.noreply.github.com>
2022-10-31 17:21:30 +0900

    Pythonの学習

--- lesson2/section18/section18-13.py
+++ lesson2/section18/section18-13.py
@@ -35,3 +35,48 @@ print(m)
 # print([w.span() for w in m])
 print([w.group() for w in m])
 
+# ? は b が 0回か1回マッチするかどうかを判定
+# m = re.match('ab?', 'a')
+# m = re.match('ab?', 'ab')
+# 2回目の b は判定されない
+m = re.match('ab?', 'abb')
+print(m)
+
+# アスタリスク(*)は0回以上の繰り返しを判定
+# m = re.match('ab*', 'a')
+# m = re.match('ab*', 'ab')
+m = re.match('ab*', 'abbbb')
+print(m)
+
+# プラス(+)は1回以上の繰り返し
+# m = re.match('ab+', 'a') == None
+# m = re.match('a+b+', 'aaabb')
+m = re.match('ab+', 'abbbbb')
+print(m)
+
+# マッチする回数を指定できる
+# m = re.match('a{3}', 'a')　== None
+m = re.match('a{3}', 'aaa')
+print(m)
+# マッチする範囲を指定できる
+m = re.match('a{2,4}', 'aaa')
+print(m)
+# 先頭からの範囲になる
+m = re.match('a{2,4}', 'aaaaa')
+print(m)
+
+# 集合(今回は a~c の範囲を指定している)
+# a~c の1文字
+# m = re.match('[a-c]', 'a')
+m = re.match('[a-c]', 'cafe')
+print(m)
+# アルファベット全体から探したい場合
+m = re.match('[a-zA-z]', 'x')
+print(m)
+# 数字を探したい場合
+m = re.match('[a-zA-z0-9]', '5')
+print(m)
+# 任意の英数もしくはアンダースコア(_)文字を探したい場合
+m = re.match('\w', '_')
+# m = re.match('\w', '@') == None
+print(m)
