e1331d0325d5f11a0ac28f2ed90626ca43b8c00b
menyuu <101458830+menyuu@users.noreply.github.com>
2022-11-10 00:35:18 +0900

    Pythonの学習

---
+++ lesson2/section22/section22-2.py
@@ -0,0 +1,26 @@
+# コルチーン
+
+# def g_hello():
+#     yield 'hello 1'
+#     yield 'hello 2'
+#     yield 'hello 3'
+#
+# for w in g_hello():
+#     print(w)
+#
+# g = g_hello()
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+def g_hello():
+    while True:
+        r = yield 'hello'
+        yield r
+
+g = g_hello()
+print(next(g))
+print(g.send('plus'))
+print(next(g))
+print(g.send('plus'))
+print(next(g))
