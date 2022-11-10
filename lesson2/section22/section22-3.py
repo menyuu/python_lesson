a42f1508d860bf29ecfbfa1c24d4102124af3dff
menyuu <101458830+menyuu@users.noreply.github.com>
2022-11-10 18:17:21 +0900

    Pythonの学習

---
+++ lesson2/section22/section22-3.py
@@ -0,0 +1,21 @@
+def s_hello():
+    yield 'hello 1'
+    yield 'hello 2'
+    yield 'hello 3'
+    return 'done'
+
+def g_hello():
+    while True:
+        # 外部のジェネレーターを呼び出す
+        r = yield from s_hello()
+        yield r
+
+g = g_hello()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
